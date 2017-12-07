fb977606a1546200b999985e7dc62c87e611b7cb
Landon Kirk <landonaddison@yahoo.com>
2017-12-07 16:39:34 -0500

    --Easy AI mode now bases it's decisions off of 66% helping players and 33% playing logically.

--- Assets/GDFR/Scripts/Controllers/AI Modules/AIModule.cs
+++ Assets/GDFR/Scripts/Controllers/AI Modules/AIModule.cs
@@ -5,51 +5,7 @@ using UnityEngine;
 public abstract class AIModule
 {
     public abstract int GetPlayValue(Card pCard, Deck toDeck, int modifier, List<Deck> playerDecks);
-
-    //public int CheckIfWouldTakeFairiesFromPlayer(List<Deck> playerDecks, Card cardInCenterDeck, int modifier)
-    //{
-    //    int count = 0;
-    //    foreach (Deck playerDeck in playerDecks)
-    //    {
-    //        Card[] playerCards = playerDeck.GetCardList();
-    //        foreach (Card playerCard in playerCards)
-    //        {
-    //            Symbol centerSymbol;
-    //            Race centerRace;
-    //            CheckAndFlipCardIfNeeded(cardInCenterDeck, playerCard, out centerRace, out centerSymbol);
-
-    //            //if the player has goblins of this symbol, we want to leave them (fairies) to help; or based on the modifier(pos for fairies, neg for goblins), we want to take them.
-    //            if (playerCard.CurrentSymbol == centerSymbol && centerRace == Race.Fairy && playerCard.CurrentRace == Race.Goblin)
-    //            {
-    //                count -= 2 * modifier;
-    //            }
-    //        }
-    //    }
-    //    return count;
-    //}
-
-    //public int CheckIfWouldAddBadGoblinsToPlayer(List<Deck> playerDecks, Card cardBeingPlayed, int modifier)
-    //{
-    //    int count = 0;
-    //    foreach (Deck playerDeck in playerDecks)
-    //    {
-    //        Card[] playerCards = playerDeck.GetCardList();
-    //        foreach (Card playerCard in playerCards)
-    //        {
-    //            Symbol centerSymbol;
-    //            Race centerRace;
-    //            CheckAndFlipCardIfNeeded(cardBeingPlayed, playerCard, out centerRace, out centerSymbol);
-
-    //            //we do not want to add goblins of the same symbol type as goblins in the player's hand to the center
-    //            if (playerCard.CurrentSymbol == centerSymbol && centerRace == Race.Goblin)
-    //            {
-    //                count -= 2 * modifier;
-    //            }
-    //        }
-    //    }
-    //    return count;
-    //}
-
+    
     //TODO: REMOVING GOBLINS SHOULD NOT BE A PLUS WHEN THE CARD CREATED THE GOBLIN IN THE FIRST PLACE(Rhyme or star card)
     public int CalculateAidToPlayers(List<Deck> playerDecks, Card cardBeingPlayed, Deck centerDeck, int modifier)
     {
--- Assets/GDFR/Scripts/Controllers/AI Modules/EasyAIModule.cs
+++ Assets/GDFR/Scripts/Controllers/AI Modules/EasyAIModule.cs
@@ -3,81 +3,29 @@ using UnityEngine;
 
 public class EasyAIModule : AIModule
 {
+    private readonly HardAIModule mHardAI;
+
+    public EasyAIModule()
+    {
+        mHardAI = new HardAIModule();
+    }
+
     public override int GetPlayValue(Card pCard, Deck toDeck, int modifier, List<Deck> playerDecks)
     {
-        int discardValue = 0;
-
-        //half of the time || SKIP IF IN SOLITAIRE MODE
-        if (Random.Range(0, 2) == 0 || Toolbox.Instance.gameSettings.numberOfPlayers == 1)
-        {
-            //make a "good" move
-            //return new HardAIModule().GetPlayValue(pCard, toDeck, modifier);
-        }
+        ////half of the time || SKIP IF IN SOLITAIRE MODE
+        //if (Random.Range(0, 2) == 0 || Toolbox.Instance.gameSettings.numberOfPlayers == 1)
+        //{
+        //    //make a "good" move
+        //    //return new HardAIModule().GetPlayValue(pCard, toDeck, modifier);
+        //}
 
         //get base play value
-        //discardValue = new HardAIModule().GetPlayValue(pCard, toDeck, modifier);
-        //int temp = discardValue;
-        //Debug.Log(pCard + " 's discard value before evaluating aid is " + discardValue);
-
-        discardValue = CalculateAidToPlayers(playerDecks, pCard, toDeck, modifier);
-        //Debug.Log(pCard + " 's discard value after evaluating aid is " + discardValue + "; aid contributed " + (discardValue-temp));
-        Debug.Log("---------------------------------------------------------------------- \n " + pCard + "'s aid value was " + discardValue);
-
-        //help the players
-
-        //Card[] tCards = toDeck.GetCardList();
-
-        //foreach (Card tCard in tCards)
-        //{
-        //    Debug.Log("Comparing card in AI deck -> " + pCard + " with card in center -> " + tCard);
-
-        //    // is it a rhyme or pCard = star?
-        //    // invert symbol and race to make the calculation
-        //    Symbol tSymbol;
-        //    Race tRace;
-        //    CheckAndFlipCardIfNeeded(tCard, pCard, out tRace, out tSymbol);
-
-        //    //is it a match?
-        //    if (tSymbol == pCard.CurrentSymbol)
-        //    {
-        //        if (pCard.CurrentRace == Race.Goblin)
-        //        {
-        //            discardValue += CheckIfWouldTakeFairiesFromPlayer(playerDecks, tCard, modifier);
-        //            Debug.Log("After checking if would take fairies from player, the discard value is " + discardValue + ". " +
-        //                      "This step contributed to this value " + CheckIfWouldTakeFairiesFromPlayer(playerDecks, tCard, modifier));
-        //        }
-        //        //is a fairy; we want to leave this if the player has this symbol
-        //        else
-        //        {
-        //            discardValue += CheckIfWouldTakeFairiesFromPlayer(playerDecks, tCard, modifier);
-        //            Debug.Log("After checking if would take fairies from player, the discard value is " + discardValue + ". " +
-        //                      "This step contributed to this value " + CheckIfWouldTakeFairiesFromPlayer(playerDecks, tCard, modifier));
-        //        }
-        //    }
-        //}
-
-        ////our card is a goblin
-        //if (pCard.CurrentRace == Race.Goblin)
-        //{
-        //    //// we dont want to add any more goblins to the center deck of the symbol types the players have as goblins
-        //    //if (discardValue == 0)
-        //    //{
-        //    //    discardValue += 1 * modifier;
-        //    //    Debug.Log("Because we don't want to add any more goblins the discard value is now " + discardValue + ". " +
-        //    //              "This step contributed to this value" + (1 * modifier));
-        //    //}
-        //    discardValue += CheckIfWouldAddBadGoblinsToPlayer(playerDecks, pCard, modifier) + modifier;
-        //    Debug.Log("After checking if this card would add bad goblins to the player, the discard value is now " + discardValue + ". " +
-        //              "This step contributed to this value" + (CheckIfWouldAddBadGoblinsToPlayer(playerDecks, pCard, modifier) + modifier));
-        //}
-        //else if (pCard.CurrentRace == Race.Fairy)
-        //{
-        //    discardValue -= 1 * modifier;
-        //    Debug.Log("Since this card is a fairy we don't want to get rid of it unless it helps the player, the discard value is now " + discardValue + ". " +
-        //              "This step contributed to this value" + -(1 * modifier));
-        //}
-
-        //Debug.Log("The final discard value for this card is " + discardValue);
+        int discardValue = mHardAI.GetPlayValue(pCard, toDeck, modifier);
+        Debug.Log(pCard + " 's discard value before evaluating aid is " + discardValue);
+        
+        //calculate aid
+        discardValue += 2 * CalculateAidToPlayers(playerDecks, pCard, toDeck, modifier);
+        Debug.Log("---------------------------------------------------------------------- \n " + pCard + "'s aid value was " + 2 * discardValue);
 
         return discardValue;
     }
