5d89a3db2253ac72d6d1c2f69eb704cfd0363015
Landon Kirk <landonaddison@yahoo.com>
2017-12-08 20:16:56 -0500

    -Background images (in menus and in the main game) no longer stretch, instead they grow with respect to their original aspect ratio.

---
+++ Assets/GDFR/Scripts/UI/BackgroundScaler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class BackgroundScaler : MonoBehaviour
+{
+    public Vector2 referenceResolution;
+
+    private RectTransform mBackgroundImage;
+    private Resolution mOldResolution;
+    private float aspectRatio;
+
+    void Awake()
+    {
+        mBackgroundImage = GetComponent<RectTransform>();
+
+        aspectRatio = referenceResolution.x / referenceResolution.y;
+    }
+
+    void Update()
+    {
+        if (mOldResolution.width != Screen.width && mOldResolution.height != Screen.height)
+        {
+            float screenAspectRatio = Screen.width / (float)Screen.height;
+            if (screenAspectRatio > aspectRatio)
+            {
+                mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,
+                    Screen.width);
+                mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
+                    Screen.width / aspectRatio);
+            }
+            else
+            {
+                mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Screen.height);
+                mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Screen.height * aspectRatio);
+            }
+
+            mOldResolution = Screen.currentResolution;
+        }
+    }
+}
