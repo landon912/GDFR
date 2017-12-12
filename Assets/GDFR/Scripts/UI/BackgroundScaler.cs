6e0226d755b4019b47e0d2229c92107aaf4d94ae
Landon Kirk <landonaddison@yahoo.com>
2017-12-12 12:17:28 -0500

    -WIP on tutorial

--- Assets/GDFR/Scripts/UI/BackgroundScaler.cs
+++ Assets/GDFR/Scripts/UI/BackgroundScaler.cs
@@ -1,40 +1,89 @@
 ﻿using UnityEngine;
 
+public enum ScaleMode
+{
+    FillLargest,
+    FitSmallest
+}
+
 [ExecuteInEditMode]
 public class BackgroundScaler : MonoBehaviour
 {
+    public ScaleMode scaleMode = ScaleMode.FillLargest;
     public Vector2 referenceResolution;
+    public int widthBuffer = 0;
+    public int heightBuffer = 0;
+    public SizeMatcher matcher;
 
     private RectTransform mBackgroundImage;
-    private Resolution mOldResolution;
+    //private Resolution mOldResolution;
     private float aspectRatio;
 
-    void Awake()
+
+    void Start()
     {
         mBackgroundImage = GetComponent<RectTransform>();
 
         aspectRatio = referenceResolution.x / referenceResolution.y;
+
     }
 
     void Update()
     {
-        if (mOldResolution.width != Screen.width && mOldResolution.height != Screen.height)
+
+        // if (mOldResolution.width != Screen.width && mOldResolution.height != Screen.height)
         {
+            float scaledWidthBuffer = widthBuffer * (Screen.width / referenceResolution.x);
+            float scaledHeightBuffer = heightBuffer * (Screen.height / referenceResolution.y);
+
             float screenAspectRatio = Screen.width / (float)Screen.height;
-            if (screenAspectRatio > aspectRatio)
+
+            if (scaleMode == ScaleMode.FillLargest)
             {
-                mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,
-                    Screen.width);
-                mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
-                    Screen.width / aspectRatio);
+                if (screenAspectRatio > aspectRatio)
+                {
+                    mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,
+                        Screen.width - scaledWidthBuffer);
+                    mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
+                        (Screen.width - scaledHeightBuffer) / aspectRatio);
+
+                    if(matcher != null)
+                        matcher.Fix(new Vector2(Screen.width, Screen.width / aspectRatio));
+                }
+                else
+                {
+                    mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Screen.height - scaledHeightBuffer);
+                    mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,
+                        (Screen.height - scaledWidthBuffer) * aspectRatio );
+
+                    if (matcher != null)
+                        matcher.Fix(new Vector2(Screen.height * aspectRatio, Screen.height));
+                }
             }
             else
             {
-                mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Screen.height);
-                mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Screen.height * aspectRatio);
+                if (screenAspectRatio > aspectRatio)
+                {
+                    mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
+                        Screen.height - scaledHeightBuffer);
+                    mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,
+                        (Screen.height - scaledWidthBuffer) * aspectRatio);
+
+                    if (matcher != null)
+                        matcher.Fix(new Vector2(Screen.height * aspectRatio, Screen.height));
+                }
+                else
+                {
+                    mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Screen.width - scaledWidthBuffer);
+                    mBackgroundImage.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
+                        (Screen.width - scaledHeightBuffer) / aspectRatio);
+
+                    if (matcher != null)
+                        matcher.Fix(new Vector2(Screen.width, Screen.width / aspectRatio));
+                }
             }
 
-            mOldResolution = Screen.currentResolution;
+            //mOldResolution = Screen.currentResolution;
         }
     }
 }
---
+++ Assets/GDFR/Scripts/UI/SizeMatcher.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class SizeMatcher : MonoBehaviour
+{
+    public Vector2 referenceSize;
+    public Vector2 referencePosition;
+
+    private Vector2 mParentReferenceSize;
+    private RectTransform mTarget;
+    //private RectTransform mParent;
+
+    void Start()
+    {
+        mTarget = GetComponent<RectTransform>();
+        //mParent = transform.parent.GetComponent<RectTransform>();
+
+        mParentReferenceSize = GetComponentInParent<BackgroundScaler>().referenceResolution;
+    }
+
+    public void Fix(Vector2 parentCurrent)
+    {
+        mTarget.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, referenceSize.x * (parentCurrent.x / mParentReferenceSize.x));
+        mTarget.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, referenceSize.y * (parentCurrent.y / mParentReferenceSize.y));
+        mTarget.localPosition = new Vector3(referencePosition.x * (parentCurrent.x / mParentReferenceSize.x), referencePosition.y * (parentCurrent.y / mParentReferenceSize.y));
+    }
+}
