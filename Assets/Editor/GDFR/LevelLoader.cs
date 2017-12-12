76d1bed2466456beb970977e756848da89fb645b
Landon Kirk <landonaddison@yahoo.com>
2017-12-12 10:00:34 -0500

    -added editor tool to make changing scenes in the editor easier

---
+++ Assets/Editor/GDFR/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class LevelLoader : ScriptableObject
+{
+    [MenuItem("GDFR/Loading Screen")]
+    static void LoadLoadingScrene()
+    {
+        EditorSceneManager.OpenScene("Assets/GDFR/Scenes/Logo.unity");
+    }
+
+    [MenuItem("GDFR/Main Menu")]
+    static void LoadMenu()
+    {
+        EditorSceneManager.OpenScene("Assets/GDFR/Scenes/MainMenu.unity");
+    }
+
+    [MenuItem("GDFR/Setup")]
+    static void LoadSetup()
+    {
+        EditorSceneManager.OpenScene("Assets/GDFR/Scenes/NewGame.unity");
+    }
+
+    [MenuItem("GDFR/Game")]
+    static void LoadGame()
+    {
+        EditorSceneManager.OpenScene("Assets/GDFR/Scenes/MainGame.unity");
+    }
+
+    
+}
